023602f57df2f312dbc7a42e2c0c7d561bf1796f
Hannes Sörensson <hannes.sorensson@gmail.com>
2021-12-01 19:56:57 +0100

    My README, first day templated

---
+++ src/aoc2021/day01.go
@@ -0,0 +1,71 @@
+package main;
+
+import (
+    "fmt"
+    "strings"
+    "aoc/libs/utils"
+);
+
+/**
+  * Start - 19:53:13
+  * p1 done - {p1Done}
+  * p2 done - {p2Done}
+  */
+
+func main() {
+    var input, _ = utils.Get_input(2021, 01);
+    // fmt.Printf("Input: %s \n", input);
+
+    var success = true;
+    for i := range part1_test_input {
+        if (part1(part1_test_input[i]) != part1_test_output[i]) {
+            success = false;
+            break;
+        }
+    }
+
+    fmt.Printf("part1 minitest success: %t! \n", success);
+    p1 := part1(input);
+    fmt.Printf("part1: %s\n\n", p1);
+
+    success = true;
+    for i := range part2_test_input {
+        if (part2(part2_test_input[i]) != part2_test_output[i]) {
+            success = false;
+            break;
+        }
+    }
+    fmt.Printf("part2 minitest success: %t! \n", success);
+    p2 := part2(input);
+    fmt.Printf("part2: %s\n", p2);
+}
+
+const separator string = "\n";
+
+var part1_test_input = []string{
+    ``,
+};
+var part1_test_output = []string{
+    ``,
+};
+func part1(input string) string {
+    var inputs = strings.Split(strings.Trim(input, separator), separator);
+
+    // ...
+
+    return "";
+}
+
+var part2_test_input = []string{
+    ``,
+};
+var part2_test_output = []string{
+    ``,
+};
+func part2(input string) string {
+    // var inputs = strings.Split(strings.Trim(input, separator), separator);
+
+    // ...
+
+    return "";
+}
