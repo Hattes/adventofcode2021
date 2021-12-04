f5260c2d1ec09f95469b9cfd4b757381160ad7eb
Hannes Sörensson <hannes.sorensson@gmail.com>
2021-12-04 13:13:24 +0100

    Wip day 4

--- src/aoc2021/day04.go
+++ src/aoc2021/day04.go
@@ -3,25 +3,25 @@ package main;
 import (
     "aoc/libs/utils"
     "fmt"
-    //"strings"
+    "strings"
     // "strconv"
 );
 
 /**
-  * Start - %{starttime}
-  * p1 done - %{p1Done}
-  * p2 done - %{p2Done}
+  * Start - 12:57:15
+  * p1 done - {p1Done}
+  * p2 done - {p2Done}
   */
 
 func main() {
-    var input, _ = utils.Get_input(%{year}, %{day});
+    var input, _ = utils.Get_input(2021, 04);
     // fmt.Printf("Input: %s \n", input);
 
     var success = true;
     for i := range part1_test_input {
         if (part1(part1_test_input[i]) != part1_test_output[i]) {
             success = false;
-            fmt.Printf("Part 1 failed with input %s: result %s != expected %s \n",
+            fmt.Printf("part1 failed with input %s: result %s != expected %s \n",
                     part1_test_input[i],
                     part1(part1_test_input[i]),
                     part1_test_output[i]);
@@ -29,37 +29,57 @@ func main() {
         }
     }
 
-    fmt.Printf("Part 1 minitest success: %t! \n", success);
+    fmt.Printf("part1 minitest success: %t! \n", success);
     p1 := part1(input);
-    fmt.Printf("Part 1: %s\n\n", p1);
+    fmt.Printf("part1: %s\n\n", p1);
 
     success = true;
     for i := range part2_test_input {
         var part2_result = part2(part2_test_input[i])
         if (part2_result != part2_test_output[i]) {
             success = false;
-            fmt.Printf("Part 2 failed with input %s: result %s != expected %s \n",
+            fmt.Printf("part2 failed with input %s: result %s != expected %s \n",
                     part2_test_input[i],
                     part2_result,
                     part2_test_output[i]);
             break;
         }
     }
-    fmt.Printf("Part 2 minitest success: %t! \n", success);
+    fmt.Printf("part2 minitest success: %t! \n", success);
     p2 := part2(input);
-    fmt.Printf("Part 2: %s\n", p2);
+    fmt.Printf("part2: %s\n", p2);
 }
 
-const separator string = "\n";
+const separator string = "\n\n";
 
 var part1_test_input = []string{
-    ``,
+    `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+`,
 };
 var part1_test_output = []string{
-    ``,
+    `4512`,
 };
 func part1(input string) string {
-    //var inputs = utils.Trim_array(strings.Split(strings.Trim(input, separator), separator));
+    var inputs = utils.Trim_array(strings.Split(strings.Trim(input, separator), separator));
+    fmt.Println(inputs)
     // var nums, _ = utils.StrToInt_array(inputs);
 
     // ...
