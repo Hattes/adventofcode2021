c4c49bc1ee0db09bd1954962f9f0eb839a1f8d7b
Hannes Sörensson <hannes.sorensson@gmail.com>
2021-12-04 12:57:28 +0100

    Day 3, part 2

--- src/aoc2021/day03.go
+++ src/aoc2021/day03.go
@@ -35,11 +35,12 @@ func main() {
 
     success = true;
     for i := range part2_test_input {
-        if (part2(part2_test_input[i]) != part2_test_output[i]) {
+        var part2_result = part2(part2_test_input[i])
+        if (part2_result != part2_test_output[i]) {
             success = false;
             fmt.Printf("part2 failed with input %s: result %s != expected %s \n",
                     part2_test_input[i],
-                    part2(part2_test_input[i]),
+                    part2_result,
                     part2_test_output[i]);
             break;
         }
@@ -111,18 +112,154 @@ func part1(input string) string {
     return strconv.Itoa(result);
 }
 
-var part2_test_input = []string{
-    ``,
-};
+var part2_test_input = part1_test_input
 var part2_test_output = []string{
-    ``,
+    `230`,
 };
+func get_template(bitcounts []BitCount) []int {
+    template := make([]int, len(bitcounts))
+    for i, bitcount := range bitcounts {
+        if bitcount.one >= bitcount.zero {
+            template[i] = 1
+        } else {
+            template[i] = 0
+        }
+    }
+    return template
+}
 func part2(input string) string {
-    // var inputs = utils.Trim_array(strings.Split(strings.Trim(input, separator), separator));
-    // var nums, _ = utils.StrToInt_array(inputs);
+    var inputs = utils.Trim_array(strings.Split(strings.Trim(input, separator), separator));
+    var length = len(inputs[0])
+    counts := make([]BitCount, length)
+    for i := range counts {
+        counts[i] = BitCount{0,0}
+    }
+    fmt.Printf("Byte length %d\n", length)
+    // Loop to get all bit counts
+    //for _, input := range inputs {
+    //    for pos, char := range input {
+    //        if char == '0' {
+    //            counts[pos].zero++
+    //        } else {
+    //            counts[pos].one++
+    //        }
+    //    }
+    //}
 
-    // ...
+    var valids_oxy = make([]bool, len(inputs))  // A number is valid for oxy if it has the most common bit in all pos
+    var valids_cot = make([]bool, len(inputs))  // Inversely, here a number should have the least common bit
+    // Init 'valid' flags
+    for i := range inputs {
+        valids_oxy[i] = true
+        valids_cot[i] = true
+    }
 
-    return "";
-    // return strconv.Itoa(result);
+    //// Get oxy
+    // Loop first over length of an input, then over each input (i.e. as described in text)
+    for i := 0; i < length; i++ {
+        for j, input := range inputs {
+            if !valids_oxy[j] {
+                continue // Has already been marked ineligible for some earlier bit, so we shouldn't count it
+            }
+            var char = input[i]
+            if char == '0' {
+                counts[i].zero++
+            } else {
+                counts[i].one++
+            }
+        }
+        for j, input := range inputs { // Loop again to use 'counts' data to set 'valids'
+            if !valids_oxy[j] {
+                continue // We should be able to skip it here too
+            }
+            var char = ([]rune(input))[i]
+            var rightchar rune
+            if counts[i].one >= counts[i].zero {
+                rightchar = '1'
+            } else {
+                rightchar = '0'
+            }
+            if char != rightchar {
+                valids_oxy[j] = false
+            }
+        }
+        // Loop over valids to see if we only have one left
+        var valid_count = 0
+        for _, valid := range valids_oxy {
+            if valid {
+                valid_count++
+            }
+        }
+        if valid_count < 2 {
+            break // we're done
+        }
+    }
+
+    var oxy int = 0
+    for i, input := range inputs { // Now loop to find the one that is valid
+        if valids_oxy[i] {
+            var res, _ = strconv.ParseInt(input, 2, 32)
+            oxy = int(res)
+            break
+        }
+    }
+    fmt.Printf("oxy %d\n", oxy)
+
+    // Now we copy-paste for co2
+    // Reset counts
+    for i := range counts {
+        counts[i] = BitCount{0,0}
+    }
+    // Loop first over length of an input, then over each input (i.e. as described in text)
+    for i := 0; i < length; i++ {
+        for j, input := range inputs {
+            if !valids_cot[j] {
+                continue // Has already been marked ineligible for some earlier bit, so we shouldn't count it
+            }
+            var char = input[i]
+            if char == '0' {
+                counts[i].zero++
+            } else {
+                counts[i].one++
+            }
+        }
+        for j, input := range inputs { // Loop again to use 'counts' data to set 'valids'
+            if !valids_cot[j] {
+                continue // We should be able to skip it here too
+            }
+            var char = ([]rune(input))[i]
+            var rightchar rune
+            if counts[i].one >= counts[i].zero {
+                rightchar = '0'
+            } else {
+                rightchar = '1'
+            }
+            if char != rightchar {
+                valids_cot[j] = false
+            }
+        }
+        // Loop over valids to see if we only have one left
+        var valid_count = 0
+        for _, valid := range valids_cot {
+            if valid {
+                valid_count++
+            }
+        }
+        if valid_count < 2 {
+            break // we're done
+        }
+    }
+    var cot int = 0
+    for i, input := range inputs { // Now loop to find the one that is valid
+        if valids_cot[i] {
+            var res, _ = strconv.ParseInt(input, 2, 32)
+            cot = int(res)
+            println("found one")
+            break
+        }
+    }
+    fmt.Printf("cot %d\n", cot)
+
+    var result = oxy * cot
+    return strconv.Itoa(result);
 }
